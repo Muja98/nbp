dc17e2ef85addc7825aa212d2fcef12eb6aa1636
cakicpredragdeveloper <cakicpredrag@outlook.com>
2020-12-26 19:54:04 +0100

    Api & Entities Created

---
+++ api/Share_To_Learn_WEB_API/Share_To_Learn_WEB_API/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Share_To_Learn_WEB_API.Entities
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string ProfilePicturePath { get; set; }
+    }
+}
