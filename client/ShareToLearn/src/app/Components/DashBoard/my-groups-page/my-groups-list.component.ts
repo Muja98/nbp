a23296ce87e7caa9f666a6e31ebffb46f4effdf8
cakicpredragdeveloper <cakicpredrag@outlook.com>
2021-01-21 16:46:05 +0100

    My Groups Page Completed 100/100

--- client/ShareToLearn/src/app/Components/DashBoard/my-groups-page/my-groups-list.component.ts
+++ client/ShareToLearn/src/app/Components/DashBoard/my-groups-page/my-groups-list.component.ts
@@ -1,3 +1,4 @@
+import { GroupService } from './../../../Service/group.service';
 import { Component, OnInit } from '@angular/core';
 import { Group } from '../../../Model/group';
 import URL from 'src/API/api'
@@ -14,40 +15,36 @@ export class MyGroupsComponent implements OnInit
     memberships:Group[];
     ownerships:Group[];
    
-    constructor(private http:HttpClient, private router:Router) { }
+    constructor(private http:HttpClient, private groupService:GroupService, private router:Router) { }
 
     getMemberships()
     {
         const userId = JSON.parse(localStorage.getItem('user'))['id']
-        return this.http.get<Group[]>(URL + "/api/groups/student/" + userId + "/memberships");
+        this.groupService.getMemberships(userId).subscribe(
+            result => {
+              debugger
+              console.log(result);
+              this.memberships= result
+            }
+         )
     }
 
     getOwnerships()
     {
         const userId = JSON.parse(localStorage.getItem('user'))['id']
-        return this.http.get<Group[]>(URL + "/api/groups/student/" + userId + "/ownerships");
-    }
-
-    ngOnInit(): void {
-        console.log("ngOnInit method");
-        debugger
-        this.getMemberships().subscribe(
-            result => {
-              debugger
-              console.log(result);
-              this.memberships= result
-              
-            }
-         )
-
-        this.getOwnerships().subscribe(
+        this.groupService.getOwnerships(userId).subscribe(
             result => {
               debugger
               console.log(result);
               this.ownerships= result
             }
         )
+    }
 
+    ngOnInit(): void {
+        console.log("ngOnInit method");
+        debugger
+        this.getOwnerships();
+        this.getMemberships();
     }
 }
-
